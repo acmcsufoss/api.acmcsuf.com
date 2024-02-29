88863e3cfed86adc948bcd58cba92e0b0cb35a80
akanksh5 <akankshjagadish@gmail.com>
2024-02-29 15:59:03 -0800

    Added the event_list_uuid to announcements

--- internal/db/sqlite/schema.sql
+++ internal/db/sqlite/schema.sql
@@ -2,15 +2,15 @@
 
 -- Create the 'resource_mappings' table.
 CREATE TABLE IF NOT EXISTS resource_group_mapping (
-    resource_uuid TEXT PRIMARY KEY REFERENCES resources(uuid),
-    group_uuid TEXT NOT NULL REFERENCES group_resource_list_mapping(uuid) ON DELETE CASCADE,
+    resource_uuid TEXT REFERENCES resources(uuid),
+    group_uuid TEXT NOT NULL REFERENCES group_resource_list_mapping(uuid),
     created_at INTEGER NOT NULL,
     updated_at INTEGER NOT NULL,
 );
 
 CREATE TABLE IF NOT EXISTS group_resource_list_mapping (
     group_uuid TEXT,
-    resource_uuid TEXT NOT NULL REFERENCES resources(uuid) ON DELETE CASCADE,
+    resource_uuid TEXT NOT NULL REFERENCES resources(uuid),
     index_in_list INTEGER NOT NULL,
     created_at INTEGER NOT NULL,
     updated_at INTEGER NOT NULL,
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS resources (
 
 -- Create the 'events' table which is a table of event resources.
 CREATE TABLE IF NOT EXISTS events (
-    uuid TEXT PRIMARY KEY REFERENCES resources(uuid) ON DELETE CASCADE,
+    uuid TEXT PRIMARY KEY REFERENCES resources(uuid),
     location TEXT NOT NULL,
     start_at NUMBER NOT NULL, -- Start time in UTC milliseconds.
     end_at NUMBER NOT NULL,
@@ -41,11 +41,11 @@ CREATE TABLE IF NOT EXISTS events (
 
 -- Create the 'announcements' table which is a table of announcement resources.
 CREATE TABLE IF NOT EXISTS announcements (
-    uuid TEXT PRIMARY KEY REFERENCES resources(uuid) ON DELETE CASCADE,
+    uuid TEXT PRIMARY KEY REFERENCES resources(uuid),
     -- no it is not resource list by uuid because one uuid may have more than one list.
     -- in this case, we have 2. One has people, one has events.
-    -- event_list_uuid TEXT REFERENCES resource_lists(uuid) ON DELETE CASCADE,
-    approved_by_list_uuid TEXT REFERENCES persons(uuid) ON DELETE CASCADE,
+    event_list_uuid TEXT REFERENCES resource_group_mapping(resource_uuid),
+    approved_by_list_uuid TEXT REFERENCES persons(uuid),
     visibility TEXT NOT NULL, -- Accepts 'public' or 'private'.
     announce_at INTEGER NOT NULL, -- UTC milliseconds.
     discord_channel_id TEXT, -- Discord channel ID. If present, the announcement has been posted.
