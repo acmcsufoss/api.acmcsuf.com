26b044d9e24a55f9232cc933a856bbb0558a8058
akanksh5 <akankshjagadish@gmail.com>
2024-03-07 18:53:16 -0800

    Added the resource person mapping table

--- internal/db/sqlite/schema.sql
+++ internal/db/sqlite/schema.sql
@@ -2,23 +2,25 @@
 
 -- Create the 'resource_mappings' table.
 CREATE TABLE IF NOT EXISTS resource_group_mapping (
-    resource_uuid TEXT REFERENCES resources(uuid),
+    resource_uuid TEXT REFERENCES resource(uuid),
     group_uuid TEXT NOT NULL REFERENCES group_resource_list_mapping(uuid),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP DEFAULT NULL,
 );
 
 CREATE TABLE IF NOT EXISTS group_resource_list_mapping (
     group_uuid TEXT,
-    resource_uuid TEXT NOT NULL REFERENCES resources(uuid),
+    resource_uuid TEXT NOT NULL REFERENCES resource(uuid),
     index_in_list INTEGER NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP DEFAULT NULL,
 );
 
 
 -- Create the 'resources' table.
-CREATE TABLE IF NOT EXISTS resources (
+CREATE TABLE IF NOT EXISTS resource (
     uuid TEXT PRIMARY KEY,
     title TEXT NOT NULL,
     content_md TEXT NOT NULL,
@@ -26,11 +28,12 @@ CREATE TABLE IF NOT EXISTS resources (
     resource_type TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP DEFAULT NULL,
 );
 
 -- Create the 'events' table which is a table of event resources.
-CREATE TABLE IF NOT EXISTS events (
-    uuid TEXT PRIMARY KEY REFERENCES resources(uuid),
+CREATE TABLE IF NOT EXISTS event (
+    uuid TEXT PRIMARY KEY REFERENCES resource(uuid),
     location TEXT NOT NULL,
     start_at NUMBER NOT NULL, -- Start time in UTC milliseconds.
     end_at NUMBER NOT NULL,
@@ -40,12 +43,12 @@ CREATE TABLE IF NOT EXISTS events (
 )
 
 -- Create the 'announcements' table which is a table of announcement resources.
-CREATE TABLE IF NOT EXISTS announcements (
-    uuid TEXT PRIMARY KEY REFERENCES resources(uuid),
+CREATE TABLE IF NOT EXISTS announcement (
+    uuid TEXT PRIMARY KEY REFERENCES resource(uuid),
     -- no it is not resource list by uuid because one uuid may have more than one list.
     -- in this case, we have 2. One has people, one has events.
     event_list_uuid TEXT REFERENCES resource_group_mapping(resource_uuid),
-    approved_by_list_uuid TEXT REFERENCES persons(uuid),
+    approved_by_list_uuid TEXT REFERENCES resource_person_mapping(uuid),
     visibility TEXT NOT NULL, -- Accepts 'public' or 'private'.
     announce_at INTEGER NOT NULL, -- UTC milliseconds.
     discord_channel_id TEXT, -- Discord channel ID. If present, the announcement has been posted.
@@ -53,11 +56,8 @@ CREATE TABLE IF NOT EXISTS announcements (
     UNIQUE (id)
 )
 
--- CREATE TABLE IF NOT EXISTS announcement_approvals (
---     uuid TEXT 
--- )
-
 -- Create the 'persons' table which is a table of person resources.
-CREATE TABLE IF NOT EXISTS persons (
-    uuid TEXT 
+CREATE TABLE IF NOT EXISTS resource_person_mapping (
+    uuid TEXT REFERENCES resource(uuid),
+    person_uuid TEXT REFERENCES event(uuid),
 )
