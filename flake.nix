46c6b4844646be328a3735193b1951df43e6c64e
josh <joshuaholman5@gmail.com>
2025-01-31 11:27:29 -0800

    modernized nix dev shell

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Generic Dev Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            gotools
+            gopls # Go langauge server
+            nilaway # Go static analysis tool
+            sqlc # compiles SQL queries to Go code
+            sqlfluff # SQL linter
+            gnumake
+            pre-commit
+          ];
+
+          shellHook = ''
+            echo "Loaded dev shell."
+          '';
+        };
+      }
+    );
+}
