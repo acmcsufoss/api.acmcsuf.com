91779480aaa60704acbc9c15d19843c037a3dfb5
Siddharth Vasu <80875915+sidvasu@users.noreply.github.com>
2025-02-15 17:22:51 -0800

    Fixed sql format issues

--- internal/db/sqlite/schema.sql
+++ internal/db/sqlite/schema.sql
@@ -1,33 +1,30 @@
 -- Language: sqlite
 
 -- Create the 'events' table which is a table of event resources.
-CREATE TABLE
-    IF NOT EXISTS event (
-        uuid TEXT PRIMARY KEY,
-        location TEXT NOT NULL,
-        start_at NUMBER NOT NULL, -- Start time in UTC milliseconds.
-        end_at NUMBER NOT NULL,
-        is_all_day BOOLEAN NOT NULL,
-        host TEXT NOT NULL, -- Accepts team ID or plain text.
-    )
+CREATE TABLE IF NOT EXISTS event (
+    uuid TEXT PRIMARY KEY,
+    location TEXT NOT NULL,
+    start_at NUMBER NOT NULL, -- Start time in UTC milliseconds.
+    end_at NUMBER NOT NULL,
+    is_all_day BOOLEAN NOT NULL,
+    host TEXT NOT NULL, -- Accepts team ID or plain text.
+);
 
 -- Create the 'person' table which is a table of person resources.
-CREATE TABLE
-    IF NOT EXISTS person (
-        uuid TEXT PRIMARY KEY,
-        name TEXT,
-        preferred_pronoun TEXT
-    )
+CREATE TABLE IF NOT EXISTS person (
+    uuid TEXT PRIMARY KEY,
+    name TEXT,
+    preferred_pronoun TEXT
+);
 
 -- Create the 'announcement' table which is a table of announcement resources.
-CREATE TABLE
-    IF NOT EXISTS announcement (
-        uuid TEXT PRIMARY KEY,
-        visibility TEXT NOT NULL, -- Accepts 'public' or 'private'.
-        announce_at INTEGER NOT NULL, -- UTC milliseconds.
-        discord_channel_id TEXT, -- Discord channel ID.
-        discord_message_id TEXT, -- Discord message ID. If present, the announcement has been posted.
-        UNIQUE (id)
-    )
+CREATE TABLE IF NOT EXISTS announcement (
+    uuid TEXT PRIMARY KEY,
+    visibility TEXT NOT NULL, -- Accepts 'public' or 'private'.
+    announce_at INTEGER NOT NULL, -- UTC milliseconds.
+    discord_channel_id TEXT, -- Discord channel ID.
+    discord_message_id TEXT, -- Discord message ID. If present, the announcement has been posted.
+    UNIQUE (id)
+);
 
 -- TODO: Create a table for access tokens for the API.
