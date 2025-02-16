719dbe93cb1f8fe8295faae315061f25a6caaa14
Siddharth Vasu <80875915+sidvasu@users.noreply.github.com>
2025-02-16 13:51:04 -0800

    Edited announcement schema

--- internal/db/sqlite/schema.sql
+++ internal/db/sqlite/schema.sql
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS announcement (
     visibility TEXT NOT NULL, -- Accepts 'public' or 'private'.
     announce_at INTEGER NOT NULL, -- UTC milliseconds.
     discord_channel_id TEXT, -- Discord channel ID.
-    discord_message_id TEXT UNIQUE, -- Discord message ID. If present, the announcement has been posted.
+    discord_message_id TEXT UNIQUE -- Discord message ID. If present, the announcement has been posted.
     --UNIQUE (id)
 );
 
