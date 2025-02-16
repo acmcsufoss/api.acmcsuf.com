fe3a7bc31a3af718430cc8acda3f6f81c786fbf2
Siddharth Vasu <80875915+sidvasu@users.noreply.github.com>
2025-02-16 13:46:52 -0800

    Edited schema

--- internal/db/sqlite/schema.sql
+++ internal/db/sqlite/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS event (
     start_at NUMBER NOT NULL, -- Start time in UTC milliseconds.
     end_at NUMBER NOT NULL,
     is_all_day BOOLEAN NOT NULL,
-    host TEXT NOT NULL, -- Accepts team ID or plain text.
+    host TEXT NOT NULL -- Accepts team ID or plain text.
 );
 
 -- Create the 'person' table which is a table of person resources.
@@ -23,8 +23,8 @@ CREATE TABLE IF NOT EXISTS announcement (
     visibility TEXT NOT NULL, -- Accepts 'public' or 'private'.
     announce_at INTEGER NOT NULL, -- UTC milliseconds.
     discord_channel_id TEXT, -- Discord channel ID.
-    discord_message_id TEXT, -- Discord message ID. If present, the announcement has been posted.
-    UNIQUE (id)
+    discord_message_id TEXT UNIQUE, -- Discord message ID. If present, the announcement has been posted.
+    --UNIQUE (id)
 );
 
 -- TODO: Create a table for access tokens for the API.
