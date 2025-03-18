d42ad16454e74aa405830d8bf9b1a29ee6387fe7
josh <joshuaholman5@gmail.com>
2025-03-18 14:23:56 -0700

    uhh

--- internal/api/services/events_service.go
+++ internal/api/services/events_service.go
@@ -2,49 +2,78 @@ package services
 
 import (
 	"github.com/swaggest/usecase"
+	"context"
+	"database/sql"
+	_ "embed"
+	"log"
+	"reflect"
 
-	"github.com/acmcsufoss/api.acmcsuf.com/internal/db/sqlite"
+	_ "modernc.org/sqlite"
+	"github.com/acmcsufoss/api.acmcsuf.com/internal/db/models"
 )
 
 type EventsService struct {
-	q *sqlite.Queries
+	q *models.Queries
+	ctx context.Background
 }
 
-func NewEventsService(q *sqlite.Queries) *EventsService {
-	return &EventsService{q}
+var ddl string
+
+func NewEventsService(q *models.Queries) (*EventsService, error) {
+	ctx := context.Background()
+
+	db, err := sql.Open("sqlite", ":memory:")
+	if err != nil {
+		return nil, err
+	}
+
+	// Create tables
+	if _, err := db.ExecContext(ctx, ddl); err != nil {
+		return nil, err
+	}
+
+	queries := models.New(db)
+	return &EventsService{queries}, nil
 }
 
-func GetEvent(q *sqlite.Queries) sqlite.Event {
+
+func GetEvent(q *models.Queries) (models.Event, error) {
 	// I think this is the wrong way to implement since this only returns error
 	// and passes around a context
-	// error := q.GetEvent(ctx context.Context, uuid string)
-	return sqlite.Event{}
+	uuid := 1 // NOTE: This is a placeholder
+	event, err := q.GetEvent(q.ctx, uuid)
+	if err != nil {
+		return nil, err
+	}
+	return event, nil
 }
 
-func (s EventsService) Resources() usecase.IOInteractor {
-	panic("implement me")
-}
+// TODO: re-implement this interface
 
-func (s EventsService) PostResources() usecase.IOInteractor {
-	panic("implement me")
-}
-
-func (s EventsService) BatchPostResources() usecase.IOInteractor {
-	panic("implement me")
-}
-
-func (s EventsService) Resource() usecase.IOInteractor {
-	panic("implement me")
-}
-
-func (s EventsService) PostResource() usecase.IOInteractor {
-	panic("implement me")
-}
-
-func (s EventsService) BatchPostResource() usecase.IOInteractor {
-	panic("implement me")
-}
-
-func (s EventsService) DeleteResource() usecase.IOInteractor {
-	panic("implement me")
-}
+// func (s EventsService) Resources() usecase.IOInteractor {
+// 	panic("implement me")
+// }
+//
+// func (s EventsService) PostResources() usecase.IOInteractor {
+// 	panic("implement me")
+// }
+//
+// func (s EventsService) BatchPostResources() usecase.IOInteractor {
+// 	panic("implement me")
+// }
+//
+// func (s EventsService) Resource() usecase.IOInteractor {
+// 	panic("implement me")
+// }
+//
+// func (s EventsService) PostResource() usecase.IOInteractor {
+// 	panic("implement me")
+// }
+//
+// func (s EventsService) BatchPostResource() usecase.IOInteractor {
+// 	panic("implement me")
+// }
+//
+// func (s EventsService) DeleteResource() usecase.IOInteractor {
+// 	panic("implement me")
+// }
