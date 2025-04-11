b7980d68aa8dcf5ebf6e801bfbaf650e55685a6c
AdamHo2028 <adamH2024@csu.fullerton.edu>
2025-04-10 17:17:12 -0700

    implemented create announcement

--- internal/api/services/announcement_service.go
+++ internal/api/services/announcement_service.go
@@ -24,8 +24,6 @@ func (s *AnnouncementService) GetAnnouncement(ctx context.Context, uuid string) 
 	return announcement, nil
 }
 
-// TODO: implement the following services
-// NOTE: these are just copy-pasted from GetEvent and need to have their interfaces modified
 func (s *AnnouncementService) CreateAnnouncement(ctx context.Context, arg models.CreateAnnouncementParams) (models.Announcement, error) {
 	if err := s.q.CreateAnnouncement(ctx, params); err != nil {
 		return err
