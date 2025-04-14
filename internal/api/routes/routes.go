307eed603ba394beeed364d21a575d2d65d5a06c
AdamHo2028 <adamH2024@csu.fullerton.edu>
2025-04-14 13:49:03 -0700

    Revert "added handlers"
    This reverts commit e838703ef1a3dce80ce607f8e30a83867c151024.

--- internal/api/routes/routes.go
+++ internal/api/routes/routes.go
@@ -6,16 +6,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func SetupRoutes(router *gin.Engine, eventService *services.EventsService, announcementService *services.AnnouncementService) {
+func SetupRoutes(router *gin.Engine, eventService *services.EventsService) {
 	eventsHandler := handlers.NewEventHandler(eventService)
+
 	// NOTE: Only GetEvent implemented so far
 	router.GET("/events", eventsHandler.GetEvents)
 	router.GET("/events/:id", eventsHandler.GetEvent)
 	router.POST("/events", eventsHandler.CreateEvent)
 	router.POST("/events/:id", eventsHandler.UpdateEvent)
 	router.DELETE("/events/:id", eventsHandler.DeleteEvent)
-	//announcementService
-	announcementHandler := handlers.NewAnnouncementHandler(announcementService)
-	router.GET("/announcement", announcementHandler.GetAnnouncement)
-	router.POST("/announcement", announcementHandler.CreateAnnouncement)
 }
