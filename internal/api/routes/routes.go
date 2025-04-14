992e0af8bcb7d9ad04d9fa8c7261cb49c356e4ac
AdamHo2028 <adamH2024@csu.fullerton.edu>
2025-04-14 14:00:27 -0700

    added routing and fixed compilation issues

--- internal/api/routes/routes.go
+++ internal/api/routes/routes.go
@@ -6,13 +6,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func SetupRoutes(router *gin.Engine, eventService *services.EventsService) {
+func SetupRoutes(router *gin.Engine, eventService *services.EventsService, announcementService *services.AnnouncementService) {
 	eventsHandler := handlers.NewEventHandler(eventService)
-
 	// NOTE: Only GetEvent implemented so far
 	router.GET("/events", eventsHandler.GetEvents)
 	router.GET("/events/:id", eventsHandler.GetEvent)
 	router.POST("/events", eventsHandler.CreateEvent)
 	router.POST("/events/:id", eventsHandler.UpdateEvent)
 	router.DELETE("/events/:id", eventsHandler.DeleteEvent)
+	//announcementService
+	announcementHandler := handlers.NewAnnouncementHandler(announcementService)
+	router.GET("/announcement", announcementHandler.GetAnnouncement)
+	router.POST("/announcement", announcementHandler.CreateAnnouncement)
 }
