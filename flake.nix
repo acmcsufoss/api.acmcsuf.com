f687fc90501c12e1b6c5b160a1b50c196f6e68f0
josh <joshuaholman5@gmail.com>
2025-04-16 19:35:45 -0700

    switch to nixpkgs latest stable release

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Go Dev Environment for api.acmcsuf.com";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
