0750372e3e740cecb2a5c1062628226763f2ae95
Siddharth Vasu <siddharth.vasu.ca@gmail.com>
2025-04-23 21:55:19 -0700

    Building board service

--- internal/api/services/board_service.go
+++ internal/api/services/board_service.go
@@ -1,3 +1,60 @@
 package services
 
-import ()
+import (
+	"context"
+
+	"github.com/acmcsufoss/api.acmcsuf.com/internal/db/models"
+)
+
+type BoardService struct {
+	q *models.Queries
+}
+
+func NewBoardService(q *models.Queries) *BoardService {
+	return &BoardService{q: q}
+}
+
+func (s *BoardService) GetOfficer(ctx context.Context, uuid string) (models.Officer, error) {
+	officer, err := s.q.GetOfficer(ctx, uuid)
+	if err != nil {
+		return models.Officer{}, err
+	}
+	return officer, nil
+}
+
+func (s *BoardService) CreateOfficer(ctx context.Context, params models.CreateOfficerParams) error {
+	if err := s.q.CreateOfficer(ctx, params); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *BoardService) GetTier(ctx context.Context, tier string) (models.Officer, error) {
+	officer, err := s.q.GetOfficer(ctx, uuid)
+	if err != nil {
+		return models.Officer{}, err
+	}
+	return officer, nil
+}
+
+func (s *BoardService) CreateTier(ctx context.Context, params models.CreateOfficerParams) error {
+	if err := s.q.CreateOfficer(ctx, params); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *BoardService) GetPosition(ctx context.Context, uuid string) (models.Officer, error) {
+	officer, err := s.q.GetOfficer(ctx, uuid)
+	if err != nil {
+		return models.Officer{}, err
+	}
+	return officer, nil
+}
+
+func (s *BoardService) CreatePosition(ctx context.Context, params models.CreateOfficerParams) error {
+	if err := s.q.CreateOfficer(ctx, params); err != nil {
+		return err
+	}
+	return nil
+}
