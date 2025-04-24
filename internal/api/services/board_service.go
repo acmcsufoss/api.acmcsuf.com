1213af0997a1ab93c3a27f8b4bb063bc73728b85
Siddharth Vasu <siddharth.vasu.ca@gmail.com>
2025-04-23 22:30:56 -0700

    Building board service

--- internal/api/services/board_service.go
+++ internal/api/services/board_service.go
@@ -29,31 +29,31 @@ func (s *BoardService) CreateOfficer(ctx context.Context, params models.CreateOf
 	return nil
 }
 
-func (s *BoardService) GetTier(ctx context.Context, tier string) (models.Officer, error) {
-	officer, err := s.q.GetOfficer(ctx, uuid)
+func (s *BoardService) GetTier(ctx context.Context, tier string) (models.Tier, error) {
+	tier, err := s.q.GetTier(ctx, tier)
 	if err != nil {
-		return models.Officer{}, err
+		return models.Tier{}, err
 	}
-	return officer, nil
+	return tier, nil
 }
 
-func (s *BoardService) CreateTier(ctx context.Context, params models.CreateOfficerParams) error {
-	if err := s.q.CreateOfficer(ctx, params); err != nil {
+func (s *BoardService) CreateTier(ctx context.Context, params models.CreateTierParams) error {
+	if err := s.q.CreateTier(ctx, params); err != nil {
 		return err
 	}
 	return nil
 }
 
-func (s *BoardService) GetPosition(ctx context.Context, uuid string) (models.Officer, error) {
-	officer, err := s.q.GetOfficer(ctx, uuid)
+func (s *BoardService) GetPosition(ctx context.Context, fullName string) (models.Position, error) {
+	position, err := s.q.GetPosition(ctx, fullName)
 	if err != nil {
-		return models.Officer{}, err
+		return models.Position{}, err
 	}
-	return officer, nil
+	return position, nil
 }
 
-func (s *BoardService) CreatePosition(ctx context.Context, params models.CreateOfficerParams) error {
-	if err := s.q.CreateOfficer(ctx, params); err != nil {
+func (s *BoardService) CreatePosition(ctx context.Context, params models.CreatePositionParams) error {
+	if err := s.q.CreatePosition(ctx, params); err != nil {
 		return err
 	}
 	return nil
