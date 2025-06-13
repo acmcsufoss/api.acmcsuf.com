2a80311b3011faedcfd7c53f6a46a68f8346eb3b
Josh Holman <josh@jolman.me>
2025-06-13 10:59:31 -0700

    use put for updating resources

--- internal/api/routes/routes.go
+++ internal/api/routes/routes.go
@@ -12,12 +12,12 @@ func SetupRoutes(router *gin.Engine, eventService *services.EventsService,
 	router.GET("/events", eventHandler.GetEvents)
 	router.GET("/events/:id", eventHandler.GetEvent)
 	router.POST("/events", eventHandler.CreateEvent)
-	router.POST("/events/:id", eventHandler.UpdateEvent)
+	router.PUT("/events/:id", eventHandler.UpdateEvent)
 	router.DELETE("/events/:id", eventHandler.DeleteEvent)
 	//announcementService
 	announcementHandler := handlers.NewAnnouncementHandler(announcementService)
 	router.GET("/announcements/:id", announcementHandler.GetAnnouncement)
 	router.POST("/announcements", announcementHandler.CreateAnnouncement)
 	router.DELETE("/announcements/:id", announcementHandler.DeleteAnnouncement)
-	router.POST("/announcements/:id", announcementHandler.UpdateAnnouncement)
+	router.PUT("/announcements/:id", announcementHandler.UpdateAnnouncement)
 }
