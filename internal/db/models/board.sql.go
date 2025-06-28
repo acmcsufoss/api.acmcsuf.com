8408e0841fba44c4aeb2d267be7af97fcffe31b8
Siddharth Vasu <siddharth.vasu.ca@gmail.com>
2025-06-28 15:32:19 -0700

    Working on GetPosition

--- internal/db/models/board.sql.go
+++ internal/db/models/board.sql.go
@@ -136,7 +136,7 @@ func (q *Queries) GetOfficer(ctx context.Context, uuid interface{}) (Officer, er
 	return i, err
 }
 
-const getPosition = `-- name: GetPosition :one
+const getPosition = `-- name: GetPosition :many
 SELECT
     oid,
     semester,
