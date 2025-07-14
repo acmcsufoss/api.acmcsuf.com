6d62b608b8f70d0fe82a13c92fdc5cea186e128a
Gaballa <lgaballal@yahoo.com>
2025-07-14 14:44:59 -0700

    add announcements subcommands to root command

--- cmd/csuf/cmd/root.go
+++ cmd/csuf/cmd/root.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/acmcsufoss/api.acmcsuf.com/internal/cli/announcements"
 	"github.com/acmcsufoss/api.acmcsuf.com/internal/cli/events"
 	"github.com/spf13/cobra"
 
