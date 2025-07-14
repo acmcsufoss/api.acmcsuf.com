b6c5ceff803377386412f9162a91fe5e837ca289
Gaballa <lgaballal@yahoo.com>
2025-07-14 14:05:08 -0700

    go -> sqltypes utility file

---
+++ utils/dbtypes/dbtypes.go
@@ -0,0 +1,26 @@
+package dbtypes
+
+import "database/sql"
+
+// Utilities for Go types to Sql null types
+
+func StringtoNullString(str string) sql.NullString {
+	return sql.NullString{
+		String: str,
+		Valid:  str != "",
+	}
+}
+
+func Int64toNullInt64(i int64) sql.NullInt64 {
+	return sql.NullInt64{
+		Int64: i,
+		Valid: i != 0,
+	}
+}
+
+func BooltoNullBool(b bool) sql.NullBool {
+	return sql.NullBool{
+		Bool:  b,
+		Valid: true,
+	}
+}
