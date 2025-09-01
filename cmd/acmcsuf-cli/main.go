501d1e56ca264deef4922e6d0f6437d9f1edb2c1
Josh Holman <josh@jolman.me>
2025-09-01 14:32:40 -0700

    rm redundant comment

--- cmd/acmcsuf-cli/main.go
+++ cmd/acmcsuf-cli/main.go
@@ -1,11 +1,5 @@
 package main
 
-// Initalizing cobra here
-// To use the CLI, first, cd into this directory (/api.acmcsuf.com/cmd/csuf)
-// Next, run: go install .
-// Now, if you have not already, export the go bin path: export PATH="$HOME/go/bin:$PATH"
-// Now type csuf in your command line and see what happens!
-
 import (
 	"log"
 	"os"
