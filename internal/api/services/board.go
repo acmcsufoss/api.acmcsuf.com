44b81c6bb6571f8d02185739d0541fce4be1c3c4
Josh Holman <josh@jolman.me>
2025-10-02 18:31:16 -0700

    use new interface

--- internal/api/services/board.go
+++ internal/api/services/board.go
@@ -7,7 +7,17 @@ import (
 )
 
 type BoardServicer interface {
-	Service[models.Officer, string, models.CreateOfficerParams, models.UpdateOfficerParams]
+	GetOfficer(ctx context.Context, id string) (models.Officer, error)
+	ListOfficers(ctx context.Context, filters ...any) ([]models.Officer, error)
+	CreateOfficer(ctx context.Context, params models.CreateOfficerParams) error
+	UpdateOfficer(ctx context.Context, id string, params models.UpdateOfficerParams) error
+	DeleteOfficer(ctx context.Context, id string) error
+
+	GetTier(ctx context.Context, tierName int64) (models.Tier, error)
+	ListTiers(ctx context.Context, filters ...any) ([]models.Tier, error)
+	// CreateTier(ctx context.Context, params models.CreateTierParams) error
+	// UpdateTier(ctx context.Context, id string, params models.UpdateTierParams) error
+	DeleteTier(ctx context.Context, tierName int64) error
 }
 
 type BoardService struct {
@@ -20,7 +30,7 @@ func NewBoardService(q *models.Queries) *BoardService {
 	return &BoardService{q: q}
 }
 
-func (s *BoardService) Get(ctx context.Context, uuid string) (models.Officer, error) {
+func (s *BoardService) GetOfficer(ctx context.Context, uuid string) (models.Officer, error) {
 	officer, err := s.q.GetOfficer(ctx, uuid)
 	if err != nil {
 		return models.Officer{}, err
@@ -28,11 +38,11 @@ func (s *BoardService) Get(ctx context.Context, uuid string) (models.Officer, er
 	return officer, nil
 }
 
-func (s *BoardService) List(ctx context.Context, filters ...any) ([]models.Officer, error) {
+func (s *BoardService) ListOfficers(ctx context.Context, filters ...any) ([]models.Officer, error) {
 	panic("not implemented")
 }
 
-func (s *BoardService) Create(ctx context.Context, params models.CreateOfficerParams) error {
+func (s *BoardService) CreateOfficer(ctx context.Context, params models.CreateOfficerParams) error {
 	err := s.q.CreateOfficer(ctx, params)
 	if err != nil {
 		return err
@@ -40,22 +50,23 @@ func (s *BoardService) Create(ctx context.Context, params models.CreateOfficerPa
 	return nil
 }
 
-func (s *BoardService) Update(ctx context.Context, uuid string, params models.UpdateOfficerParams) error {
+func (s *BoardService) UpdateOfficer(ctx context.Context, uuid string, params models.UpdateOfficerParams) error {
 	return nil
 }
 
-func (s *BoardService) Delete(ctx context.Context, uuid string) error {
+func (s *BoardService) DeleteOfficer(ctx context.Context, uuid string) error {
 	return nil
 }
 
-// func (s *BoardService) GetTier(ctx context.Context, tierName int64) (models.Tier, error) {
-// 	tier, err := s.q.GetTier(ctx, tierName)
-// 	if err != nil {
-// 		return models.Tier{}, err
-// 	}
-// 	return tier, nil
-// }
-//
+func (s *BoardService) GetTier(ctx context.Context, tierName int64) (models.Tier, error) {
+	// tier, err := s.q.GetTier(ctx, tierName)
+	// if err != nil {
+	// 	return models.Tier{}, err
+	// }
+	// return tier, nil
+	panic("not implemented")
+}
+
 // func (s *BoardService) CreateTier(ctx context.Context, params models.CreateTierParams) error {
 // 	err := s.q.CreateTier(ctx, params)
 // 	if err != nil {
@@ -63,7 +74,15 @@ func (s *BoardService) Delete(ctx context.Context, uuid string) error {
 // 	}
 // 	return nil
 // }
-//
+
+func (s *BoardService) ListTiers(ctx context.Context, filters ...any) ([]models.Tier, error) {
+	panic("not implemented")
+}
+
+func (s *BoardService) DeleteTier(ctx context.Context, tierName int64) error {
+	panic("not implemented")
+}
+
 // func (s *BoardService) GetPosition(ctx context.Context, fullName string) (models.Position, error) {
 // 	position, err := s.q.GetPosition(ctx, fullName)
 // 	if err != nil {
@@ -71,7 +90,7 @@ func (s *BoardService) Delete(ctx context.Context, uuid string) error {
 // 	}
 // 	return position, nil
 // }
-//
+
 // func (s *BoardService) CreatePosition(ctx context.Context, params models.CreatePositionParams) error {
 // 	err := s.q.CreatePosition(ctx, params)
 // 	if err != nil {
