7473d1c98a4f065fb67f8d3d6c34b5f3ab33e04c
Mark Gaballa <mgaballa@csu.fullerton.edu>
2025-11-13 11:33:05 -0800

    Add comment to schema.sql

--- internal/db/sql/schemas/schema.sql
+++ internal/db/sql/schemas/schema.sql
@@ -1,5 +1,10 @@
 -- Language: sqlite
 
+-- Note: Using datatypes the sqlc does not know how to interpret (such as INT instead of INTEGER)
+-- will result in sqlc generating interfaces, which is undesired. I recommend skimming the docs
+-- if you are unsure about your sqlc: 
+-- https://docs.sqlc.dev/en/stable/reference/datatypes.html
+
 -- Create the 'events' table which is a table of event resources.
 CREATE TABLE IF NOT EXISTS event (
     uuid TEXT PRIMARY KEY,
