9700c73692edf7d2b90f32e81e58671758beaac0
Josh Holman <git@jolman.me>
2025-11-21 10:14:58 -0800

    add config

---
+++ internal/api/config/config.go
@@ -0,0 +1,17 @@
+package config
+
+import "github.com/acmcsufoss/api.acmcsuf.com/utils"
+
+type Config struct {
+	Port           string
+	DatabaseURL    string
+	AllowedOrigins []string
+}
+
+func Load() *Config {
+	return &Config{
+		Port:           utils.GetEnv("PORT", "8080"),
+		AllowedOrigins: utils.GetEnvAsSlice("CORS_ALLOWED_ORIGINS", []string{"*"}),
+		DatabaseURL:    utils.GetEnv("DATABASE_URL", "file:dev.db?cache=shared&mode=rwc"),
+	}
+}
---
+++ utils/get_env.go
@@ -0,0 +1,33 @@
+package utils
+
+import (
+	"os"
+	"strings"
+	"strconv"
+)
+
+func GetEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+
+	return defaultValue
+}
+
+func GetEnvAsSlice(key string, defaultValue []string) []string {
+	if value := os.Getenv(key); value != "" {
+		return strings.Split(value, ",")
+	}
+
+	return defaultValue
+}
+
+func GetBoolEnv(key string, defaultValue bool) bool {
+	if value := os.Getenv(key); value != "" {
+		if parsed, err := strconv.ParseBool(value); err == nil {
+			return parsed
+		}
+	}
+
+	return defaultValue
+}
