f05f0c3be479caffa9046776cead9ea28a2cb11b
Josh Holman <git@jolman.me>
2025-11-22 20:42:15 -0800

    add ENV var

--- internal/api/config/config.go
+++ internal/api/config/config.go
@@ -3,6 +3,7 @@ package config
 import "github.com/acmcsufoss/api.acmcsuf.com/utils"
 
 type Config struct {
+	Env            string
 	Port           string
 	DatabaseURL    string
 	TrustedProxies []string
@@ -11,6 +12,7 @@ type Config struct {
 
 func Load() *Config {
 	return &Config{
+		Env:			utils.GetEnv("ENV", "development"),
 		Port:           utils.GetEnv("PORT", "8080"),
 		DatabaseURL:    utils.GetEnv("DATABASE_URL", "file:dev.db?cache=shared&mode=rwc"),
 		TrustedProxies: utils.GetEnvAsSlice("TRUSTED_PROXIES", []string{"127.0.0.1/32"}),
