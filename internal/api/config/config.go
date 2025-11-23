96a737833111f4b26125e0ce0dbdf6618d148c7c
Josh Holman <git@jolman.me>
2025-11-22 21:23:46 -0800

    init stuff

--- internal/api/config/config.go
+++ internal/api/config/config.go
@@ -8,14 +8,16 @@ type Config struct {
 	DatabaseURL    string
 	TrustedProxies []string
 	AllowedOrigins []string
+	GuildID        string
 }
 
 func Load() *Config {
 	return &Config{
-		Env:			utils.GetEnv("ENV", "development"),
+		Env:            utils.GetEnv("ENV", "development"),
 		Port:           utils.GetEnv("PORT", "8080"),
 		DatabaseURL:    utils.GetEnv("DATABASE_URL", "file:dev.db?cache=shared&mode=rwc"),
 		TrustedProxies: utils.GetEnvAsSlice("TRUSTED_PROXIES", []string{"127.0.0.1/32"}),
 		AllowedOrigins: utils.GetEnvAsSlice("ALLOWED_ORIGINS", []string{"*"}),
+		GuildID:        utils.GetEnv("GUILD_ID", "710225099923521558"),
 	}
 }
