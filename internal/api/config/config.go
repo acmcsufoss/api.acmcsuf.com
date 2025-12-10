5b62db8729b174361ab9080c8f5b9c54a24949a2
Josh Holman <j@jolman.me>
2025-12-10 11:40:16 -0800

    Revert "Implement Discord OAuth Middleware (#125)"
    This reverts commit 2737256a4ccd25cbc0f5374ba6f96f26d4101d67.

--- internal/api/config/config.go
+++ internal/api/config/config.go
@@ -1,32 +1,19 @@
 package config
 
-import (
-	"os"
-
-	"github.com/acmcsufoss/api.acmcsuf.com/utils"
-)
+import "github.com/acmcsufoss/api.acmcsuf.com/utils"
 
 type Config struct {
-	Env             string
-	Port            string
-	DatabaseURL     string
-	TrustedProxies  []string
-	AllowedOrigins  []string
-	GuildID         string
-	DiscordBotToken string
+	Port           string
+	DatabaseURL    string
+	TrustedProxies []string
+	AllowedOrigins []string
 }
 
 func Load() *Config {
 	return &Config{
-		// Non-sensistive
-		Env:            utils.GetEnv("ENV", "development"),
 		Port:           utils.GetEnv("PORT", "8080"),
 		DatabaseURL:    utils.GetEnv("DATABASE_URL", "file:dev.db?cache=shared&mode=rwc"),
 		TrustedProxies: utils.GetEnvAsSlice("TRUSTED_PROXIES", []string{"127.0.0.1/32"}),
 		AllowedOrigins: utils.GetEnvAsSlice("ALLOWED_ORIGINS", []string{"*"}),
-		GuildID:        utils.GetEnv("GUILD_ID", "710225099923521558"), // acmcsuf's GuildID
-
-		// Sensitive (no default)
-		DiscordBotToken: os.Getenv("DISCORD_BOT_TOKEN"),
 	}
 }
