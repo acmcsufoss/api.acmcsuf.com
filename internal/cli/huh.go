f9d0126f53be703cb5743873a494c330a9984c35
AdamHo2028 <adamH2024@csu.fullerton.edu>
2025-12-27 18:31:00 -0800

    last min tweaks

--- internal/cli/huh.go
+++ internal/cli/huh.go
@@ -5,6 +5,8 @@ import (
 	"os"
 
 	"github.com/acmcsufoss/api.acmcsuf.com/internal/cli/announcements"
+	"github.com/acmcsufoss/api.acmcsuf.com/internal/cli/boards/officers"
+	"github.com/acmcsufoss/api.acmcsuf.com/internal/cli/events"
 	"github.com/charmbracelet/huh"
 )
 
@@ -24,6 +26,7 @@ func beginning() {
 				Options(
 					huh.NewOption("View Commands", "commands"),
 					huh.NewOption("Learn More about the API", "flags"),
+					huh.NewOption("Exit", "exit"),
 				).
 				Value(&state.menuOptions),
 		),
@@ -36,9 +39,11 @@ func beginning() {
 	}
 	if state.menuOptions == "commands" {
 		commands()
-	} else {
+	}
+	if state.menuOptions == "flags" {
 		about()
 	}
+	if state.menuOptions == "exit" {}
 }
 
 // Commands for CLI option
@@ -56,10 +61,9 @@ func commands() {
 				Description("Choose a command to your heart's content.").
 				Options(
 					huh.NewOption("Announcements", "announcements"),
-					huh.NewOption("Completion", "completion"),
+					huh.NewOption("Officers", "officers"),
 					huh.NewOption("Events", "events"),
 					huh.NewOption("Help", "help"),
-					huh.NewOption("Officers", "officers"),
 					huh.NewOption("Back", "back"),
 				).
 				Value(&commandState.options),
@@ -75,6 +79,15 @@ func commands() {
 	}
 	if commandState.options == "announcements" {
 		announcementsMenu()
+	}
+	if commandState.options == "officers" {
+		boardMenu()
+	}
+	if commandState.options == "events" {
+		eventsMenu()
+	}
+	if commandState.options == "completion" {
+
 	}
 }
 
@@ -149,11 +162,11 @@ func announcementsMenu() {
 			Value(&uuid).
 			Run()
 		announcements.CLIAnnouncements.SetArgs([]string{"delete", "--id", uuid})
-		announcements.GetAnnouncement.Execute()
+		announcements.CLIAnnouncements.Execute()
 	}
 	if announcementState.options == "get" {
 		announcements.CLIAnnouncements.SetArgs([]string{"get"})
-		announcements.GetAnnouncement.Execute()
+		announcements.CLIAnnouncements.Execute()
 	}
 	if announcementState.options == "post" {
 		var uuid string
@@ -216,7 +229,227 @@ func announcementsMenu() {
 		announcements.CLIAnnouncements.Execute()
 
 	}
+	beginning()
 }
+
+type boardOptions struct {
+	options string
+}
+
+func boardMenu() {
+	var boardState boardOptions
+	boardMenu := huh.NewForm(
+		huh.NewGroup(
+			huh.NewSelect[string]().
+				//Ask the user what commands they want to use.
+				Title("ACMCSUF-CLI Boards").
+				Description("Choose an option to your heart's content.").
+				Options(
+					huh.NewOption("Delete", "delete"),
+					huh.NewOption("Get", "get"),
+					huh.NewOption("Post", "post"),
+					huh.NewOption("Put", "put"),
+					huh.NewOption("Back", "back"),
+				).
+				Value(&boardState.options),
+		),
+	)
+	err := boardMenu.Run()
+	if err != nil {
+		fmt.Println("Uh oh:", err)
+		os.Exit(1)
+	}
+	if boardState.options == "delete"{
+		var uuid string
+		huh.NewInput().
+			Title("ACMCSUF-CLI Board Delete:").
+			Description("Please enter the officer's uuid:").
+			Prompt("> ").
+			Value(&uuid).
+			Run()
+		officers.CLIOfficers.SetArgs([]string{"delete", "--id", uuid})
+		officers.CLIOfficers.Execute()
+	}
+	if boardState.options == "get" {
+		officers.CLIOfficers.SetArgs([]string{"get"})
+		officers.CLIOfficers.Execute()
+	}
+	if boardState.options == "post" {
+		var uuid string
+		var fullName string
+		var pictureLink string
+		var githubLink string
+		var discordLink string
+		huh.NewInput().
+			Title("ACMCSUF-CLI Board Post:").
+			Description("Please enter officer's uuid:").
+			Prompt("> ").
+			Value(&uuid).
+			Run()
+		huh.NewInput().
+			Title("ACMCSUF-CLI Board Post:").
+			Description("Please enter the officer's full name:").
+			Prompt("> ").
+			Value(&fullName).
+			Run()
+		huh.NewInput().
+			Title("ACMCSUF-CLI Board Post:").
+			Description("Please enter the picture link for officer:").
+			Prompt("> ").
+			Value(&pictureLink).
+			Run()
+		huh.NewInput().
+			Title("ACMCSUF-CLI Board Post:").
+			Description("Please enter the github link for officer:").
+			Prompt("> ").
+			Value(&githubLink).
+			Run()
+		huh.NewInput().
+			Title("ACMCSUF-CLI Board Post:").
+			Description("Please enter the discord link for officer").
+			Prompt("> ").
+			Value(&discordLink).
+			Run()
+		officers.CLIOfficers.SetArgs([]string{"post", "--uuid", uuid, "-n", fullName, "-p", pictureLink, "-g", githubLink, "-d", discordLink})
+		officers.CLIOfficers.Execute()
+	}
+	if boardState.options == "put" {
+		var uuid string
+		valid := false
+		args := []string{"put"}
+		for !valid {
+			huh.NewInput().
+				Title("ACMCSUF-CLI Board Put:").
+				Description("Please enter the officer's uuid:").
+				Prompt("> ").
+				Value(&uuid).
+				Run()
+			if uuid != "" {
+				valid = true
+				args = append(args, "--id", uuid)
+			} else {
+			}
+		}
+		officers.CLIOfficers.SetArgs(args)
+		officers.CLIOfficers.Execute()
+
+	}
+	beginning()
+}
+type eventOptions struct {
+	options string
+}
+func eventsMenu() {
+	var eventState eventOptions
+	eventMenu := huh.NewForm(
+		huh.NewGroup(
+			huh.NewSelect[string]().
+				//Ask the user what commands they want to use.
+				Title("ACMCSUF-CLI Menu").
+				Description("Choose an option to your heart's content.").
+				Options(
+					huh.NewOption("Delete", "delete"),
+					huh.NewOption("Get", "get"),
+					huh.NewOption("Post", "post"),
+					huh.NewOption("Put", "put"),
+					huh.NewOption("Back", "back"),
+				).
+				Value(&eventState.options),
+		),
+	)
+	err := eventMenu.Run()
+	if err != nil {
+		fmt.Println("Uh oh:", err)
+		os.Exit(1)
+	}
+	if eventState.options == "delete"{
+		var uuid string
+		huh.NewInput().
+			Title("ACMCSUF-CLI Event Delete:").
+			Description("Please enter the events's uuid:").
+			Prompt("> ").
+			Value(&uuid).
+			Run()
+		events.CLIEvents.SetArgs([]string{"delete", "--id", uuid})
+		events.CLIEvents.Execute()
+	}
+	if eventState.options == "get" {
+		events.CLIEvents.SetArgs([]string{"get"})
+		events.CLIEvents.Execute()
+	}
+	if eventState.options == "post" {
+		var uuid string
+		var location string
+		var timeStart string
+		var duration string
+		var allDayYes string
+		var host string
+		huh.NewInput().
+			Title("ACMCSUF-CLI Event Post:").
+			Description("Please enter event's uuid:").
+			Prompt("> ").
+			Value(&uuid).
+			Run()
+		huh.NewInput().
+			Title("ACMCSUF-CLI Event Post:").
+			Description("Please enter the event's location:").
+			Prompt("> ").
+			Value(&location).
+			Run()
+		huh.NewInput().
+			Title("ACMCSUF-CLI Event Post:").
+			Description("Please enter the start time of the event in the following format:\n [Month]/[Day]/[Year] [Hour]:[Minute][PM | AM]\nFor example: \x1b[93m01/02/06 03:04PM\x1b[0m").
+			Prompt("> ").
+			Value(&timeStart).
+			Run()
+		huh.NewInput().
+			Title("ACMCSUF-CLI Event Post:").
+			Description("Please enter the duration of the event in the following format:\n [Hour]:[Minute]\nFor example: \x1b[93m03:04\x1b[0m").
+			Prompt("> ").
+			Value(&duration).
+			Run()
+		huh.NewSelect[string]().
+			Title("ACMCSUF-CLI Event Post:").
+			Description("Is your event all day?").
+			Options(
+				huh.NewOption("Yes", "yes"),
+				huh.NewOption("No", "n"),
+			).
+			Value(&allDayYes).
+			Run()
+		huh.NewInput().
+			Title("ACMCSUF-CLI Event Post:").
+			Description("Please enter the event host").
+			Prompt("> ").
+			Value(&host).
+			Run()
+		events.CLIEvents.SetArgs([]string{"post", "--uuid", uuid, "-l", location, "-s", timeStart, "-d", duration, "-a", allDayYes, "-H", host})
+		events.CLIEvents.Execute()
+	}
+	if eventState.options == "put" {
+		var uuid string
+		valid := false
+		args := []string{"put"}
+		for !valid {
+			huh.NewInput().
+				Title("ACMCSUF-CLI Event Put:").
+				Description("Please enter the events's uuid:").
+				Prompt("> ").
+				Value(&uuid).
+				Run()
+			if uuid != "" {
+				valid = true
+				args = append(args, "--id", uuid)
+			} else {
+			}
+		}
+		events.CLIEvents.SetArgs(args)
+		events.CLIEvents.Execute()
+	}
+	beginning()
+}
+
+
 func main() {
 	beginning()
 }
