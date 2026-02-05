36499f523965b17bcfc5826ee436197ca35db9ad
Mark Gaballa <mgaballa@csu.fullerton.edu>
2026-02-05 09:29:58 -0800

    Rate limiter no longer runs on development env

--- internal/api/server.go
+++ internal/api/server.go
@@ -34,7 +34,10 @@ func Run(ctx context.Context) {
 	announcementService := services.NewAnnouncementService(queries)
 	boardService := services.NewBoardService(queries, db)
 	router := gin.Default()
-	router.Use(mw.Cors(), mw.Ratelimiter())
+	router.Use(mw.Cors())
+	if cfg.Env != "development" {
+		router.Use(mw.Ratelimiter())
+	}
 
 	router.SetTrustedProxies(cfg.TrustedProxies)
 	routes.SetupRoot(router)
