69d0368698d2b4c8e1f9ef7be6df22a2d9381ac3
Josh Holman <git@jolman.me>
2026-02-10 11:23:44 -0800

    scaffold announcement/delete

--- internal/cli/announcements/delete.go
+++ internal/cli/announcements/delete.go
@@ -20,31 +20,17 @@ var DeleteAnnouncements = &cobra.Command{
 	Short: "delete an announcement by its id",
 
 	Run: func(cmd *cobra.Command, args []string) {
-		var uuidVal string
-		cmd.Flags().Set("id", uuidVal)
-		err := huh.NewForm().Run()
-		if err != nil {
-			if err == huh.ErrUserAborted {
-				fmt.Println("User canceled the form — exiting.")
-			}
-			fmt.Println("Uh oh:", err)
-			os.Exit(1)
+		var uuid string
+
+		if cmd.Flags().Changed("id") {
+			uuid, _ = cmd.Flags().GetString("id")
+		} else {
+			uuid = getIdInteractive()
 		}
-		err = huh.NewInput().
-			Title("ACMCSUF-CLI Announcement Delete:").
-			Description("Please enter the announcement's ID:").
-			Prompt("> ").
-			Value(&uuidVal).
-			Run()
-		if err != nil {
-			if err == huh.ErrUserAborted {
-				fmt.Println("User canceled the form — exiting.")
-			}
-			fmt.Println("Uh oh:", err)
-			os.Exit(1)
+		if uuid == "" {
+			fmt.Println("Error: no ID specified")
+			return
 		}
-		cmd.Flags().Set("id", uuidVal)
-		uuid, _ := cmd.Flags().GetString("id")
 		deleteAnnouncement(uuid, config.Cfg)
 	},
 }
@@ -59,53 +45,35 @@ func deleteAnnouncement(id string, cfg *config.Config) {
 		Scheme: "http",
 		Host:   fmt.Sprintf("%s:%s", cfg.Host, cfg.Port),
 	}
-	if err := utils.CheckConnection(baseURL.JoinPath("/health").String()); err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	if id == "" {
-		fmt.Println("ID is required to delete an announcement! Please use the --id flag")
-		return
-	}
-
-	// ----- Constructing Url -----
 	deleteUrl := baseURL.JoinPath("v1/announcements/", id)
 
 	// ----- Delete -----
 	request, err := oauth.NewRequestWithAuth(http.MethodDelete, deleteUrl.String(), nil)
 	if err != nil {
-		fmt.Println("error with delete request:", err)
+		fmt.Println("Error: failed to construct delete request:", err)
 		return
 	}
 
 	client := http.Client{}
-
 	response, err := client.Do(request)
 	if err != nil {
-		fmt.Println("error with delete response:", err)
+		fmt.Println("Error: failed to send delete request:", err)
 		return
 	}
-	if response.StatusCode != http.StatusOK {
-		fmt.Println("response status:", response.Status)
-		return
-	}
-
-	if response == nil {
-		fmt.Println("no response received")
-		return
-	}
-
 	defer response.Body.Close()
 
-	// ----- Read Response Information -----
-	fmt.Println("Request status:", response.Status)
-
-	body, err := io.ReadAll(response.Body)
-	if err != nil {
-		fmt.Println("error with reading body:", err)
+	if response.StatusCode != http.StatusOK {
+		fmt.Println("Error: HTTP ", response.Status)
 		return
 	}
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("Error: failed to read response body:", err)
+		return
+	}
+	utils.PrettyPrintJSON(body)
+}
 
-	fmt.Println(string(body))
+func getIdInteractive() string {
+	return ""
 }
