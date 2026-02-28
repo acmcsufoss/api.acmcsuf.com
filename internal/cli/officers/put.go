9ca93493d7df7fdd7183658686279c0d1ac667cd
Josh Holman <git@jolman.me>
2026-02-28 14:17:54 -0800

    fixup officer put

--- internal/cli/officers/put.go
+++ internal/cli/officers/put.go
@@ -1,22 +1,18 @@
 package officers
 
 import (
-	"bufio"
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
-	"net/url"
 	"os"
-	"strings"
 
 	"github.com/charmbracelet/huh"
 	"github.com/spf13/cobra"
 
 	"github.com/acmcsufoss/api.acmcsuf.com/internal/api/dbmodels"
+	"github.com/acmcsufoss/api.acmcsuf.com/internal/cli/client"
 	"github.com/acmcsufoss/api.acmcsuf.com/internal/cli/config"
-	"github.com/acmcsufoss/api.acmcsuf.com/internal/cli/oauth"
 	"github.com/acmcsufoss/api.acmcsuf.com/utils"
 )
 
@@ -25,268 +21,85 @@ var PutOfficer = &cobra.Command{
 	Short: "update an existing officer by id",
 
 	Run: func(cmd *cobra.Command, args []string) {
-		payload := dbmodels.UpdateOfficerParams{}
-		var uuidVal string
-		cmd.Flags().Set("id", uuidVal)
-		huh.NewForm().Run()
-		huh.NewInput().
-			Title("ACMCSUF-CLI Officer Put:").
-			Description("Please enter the officer's ID:").
-			Prompt("> ").
-			Value(&uuidVal).
-			Run()
-		cmd.Flags().Set("id", uuidVal)
 		id, _ := cmd.Flags().GetString("id")
-
-		fullname, _ := cmd.Flags().GetString("fullname")
-		picture, _ := cmd.Flags().GetString("picture")
-		github, _ := cmd.Flags().GetString("github")
-		discord, _ := cmd.Flags().GetString("discord")
-		uuid, _ := cmd.Flags().GetString("uuid")
-
-		payload.FullName = fullname
-		payload.Picture = utils.StringtoNullString(picture)
-		payload.Github = utils.StringtoNullString(github)
-		payload.Discord = utils.StringtoNullString(discord)
-		payload.Uuid = uuid
-
-		flags := officerFlags{
-			fullname: cmd.Flags().Lookup("fullname").Changed,
-			picture:  cmd.Flags().Lookup("picture").Changed,
-			github:   cmd.Flags().Lookup("github").Changed,
-			discord:  cmd.Flags().Lookup("discord").Changed,
-			uuid:     cmd.Flags().Lookup("uuid").Changed,
-		}
-
-		putOfficer(id, &payload, flags, config.Cfg)
+		putOfficer(id, config.Cfg)
 	},
 }
 
 func init() {
 	PutOfficer.Flags().String("id", "", "Officer ID to update")
-
-	PutOfficer.Flags().String("fullname", "", "Change full name")
-	PutOfficer.Flags().String("picture", "", "Change picture URL")
-	PutOfficer.Flags().String("github", "", "Change GitHub username")
-	PutOfficer.Flags().String("discord", "", "Change Discord tag")
-	PutOfficer.Flags().String("uuid", "", "Change uuid")
-
 	PutOfficer.MarkFlagRequired("id")
 }
 
-func putOfficer(id string, payload *dbmodels.UpdateOfficerParams, flags officerFlags, cfg *config.Config) {
-	baseURL := &url.URL{
-		Scheme: "http",
-		Host:   fmt.Sprintf("%s:%s", cfg.Host, cfg.Port),
-	}
-	if err := utils.CheckConnection(baseURL.JoinPath("/health").String()); err != nil {
-		fmt.Println(err)
-		return
-	}
+func putOfficer(id string, cfg *config.Config) {
+	url := config.GetBaseURL(cfg).JoinPath("v1", "board", "officers", id)
 
-	if id == "" {
-		fmt.Println("Officer id required for put!")
-		return
-	}
-
-	// construct url
-	u := baseURL.JoinPath("v1/board/officers/", id)
-
-	// getting old officer
-	client := http.Client{}
-	getReq, err := oauth.NewRequestWithAuth(http.MethodGet, u.String(), nil)
-	if err != nil {
-		fmt.Printf("error making request %s: %s\n", id, err)
-		return
-	}
-
-	resp, err := client.Do(getReq)
-	if err != nil {
-		fmt.Println("error getting response", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Println("get response code:", resp.Status)
-		return
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("error reading response body:", err)
-		return
-	}
-
-	var old dbmodels.CreateOfficerParams
-	if err := json.Unmarshal(body, &old); err != nil {
-		fmt.Println("error unmarshaling previous officer data:", err)
-		return
-	}
-
-	scanner := bufio.NewScanner(os.Stdin)
-
-	// full name
-	for {
-		if flags.fullname {
-			break
-		}
-
-		change, err := utils.ChangePrompt("full name", old.FullName, scanner, "officer")
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		if change != nil {
-			payload.FullName = string(change)
-		} else {
-			payload.FullName = old.FullName
-		}
-		break
-	}
-
-	// picture
-	for {
-		if flags.picture {
-			break
-		}
-
-		change, err := utils.ChangePrompt("picture", old.Picture.String, scanner, "officer")
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		if change != nil {
-			payload.Picture = utils.StringtoNullString(string(change))
-		} else {
-			payload.Picture = old.Picture
-		}
-		break
-	}
-
-	// github
-	for {
-		if flags.github {
-			break
-		}
-
-		change, err := utils.ChangePrompt("github", old.Github.String, scanner, "officer")
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		if change != nil {
-			payload.Github = utils.StringtoNullString(string(change))
-		} else {
-			payload.Github = old.Github
-		}
-		break
-	}
-
-	// discord
-	for {
-		if flags.discord {
-			break
-		}
-
-		change, err := utils.ChangePrompt("discord", old.Discord.String, scanner, "officer")
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		if change != nil {
-			payload.Discord = utils.StringtoNullString(string(change))
-		} else {
-			payload.Discord = old.Discord
-		}
-		break
-	}
-
-	// uuid
-	for {
-		if flags.uuid {
-			break
-		}
-
-		change, err := utils.ChangePrompt("uuid", old.Uuid, scanner, "officer")
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		if change != nil {
-			payload.Uuid = string(change)
-		} else {
-			payload.Uuid = old.Uuid
-		}
-		break
-	}
-
-	// Confirm
-	for {
-		var option string
-		description := "Is your board data correct?\n" + utils.PrintStruct(payload)
-		huh.NewSelect[string]().
-			Title("ACMCSUF-CLI Officer Put:").
-			Description(description).
-			Options(
-				huh.NewOption("Yes", "yes"),
-				huh.NewOption("No", "n"),
-			).
-			Value(&option).
-			Run()
-		scanner := bufio.NewScanner(strings.NewReader(option))
-		utils.PrintStruct(payload)
-		scanner.Scan()
-		confirmation := scanner.Bytes()
-
-		ok, err := utils.YesOrNo(confirmation, scanner)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		if !ok {
+	// ----- Get officer we want to update
+	var oldPayload dbmodels.UpdateOfficerParams
+	if body, err := client.SendRequestAndReadResponse(url, http.MethodGet, nil); err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
+	} else {
+		if err := json.Unmarshal(body, &oldPayload); err != nil {
+			fmt.Fprintln(os.Stderr, "Error: failed to unmarshal response body:", err)
 			return
 		}
-		break
 	}
 
-	// marshal payload
-	jsonPayload, err := json.Marshal(*payload)
+	// ----- Update found officer -----
+	// Read new data
+	newPayload, err := putForm(id)
 	if err != nil {
-		fmt.Println("Error marshaling data:", err)
+		fmt.Fprintln(os.Stderr, "Error:", err)
 		return
 	}
-
-	// PUT
-	putReq, err := oauth.NewRequestWithAuth(http.MethodPut, u.String(), bytes.NewBuffer(jsonPayload))
+	b, err := json.Marshal(newPayload)
 	if err != nil {
-		fmt.Println("Problem with PUT:", err)
+		fmt.Fprintln(os.Stderr, "Error: failed to marshal data:", err)
 		return
 	}
 
-	putResp, err := client.Do(putReq)
-	if err != nil {
-		fmt.Println("Error with response:", err)
-		return
+	// Update remote resource with new data
+	if body, err := client.SendRequestAndReadResponse(url, http.MethodDelete,
+		bytes.NewBuffer(b)); err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
+	} else {
+		utils.PrettyPrintJSON(body)
 	}
-	if putResp == nil {
-		fmt.Println("no response received")
-		return
+}
+
+func putForm(uuid string) (*dbmodels.UpdateOfficerParams, error) {
+	var payload dbmodels.UpdateOfficerParams
+	var err error
+	var (
+		picture string
+		github  string
+		discord string
+	)
+
+	form := huh.NewForm(
+		huh.NewGroup(
+			huh.NewInput().
+				Title("Full Name").
+				Value(&payload.FullName),
+			huh.NewInput().
+				Title("Picture URL").
+				Value(&picture),
+			huh.NewInput().
+				Title("GitHub URL").
+				Value(&github),
+			huh.NewInput().
+				Title("Discord URL").
+				Value(&discord),
+		),
+	)
+	if err = form.Run(); err != nil {
+		return nil, err
 	}
-	defer putResp.Body.Close()
 
-	if putResp.StatusCode != http.StatusOK {
-		fmt.Println("put response status:", putResp.Status)
-		return
-	}
+	payload.Uuid = uuid
+	payload.Picture = utils.StringtoNullString(picture)
+	payload.Github = utils.StringtoNullString(github)
+	payload.Discord = utils.StringtoNullString(discord)
 
-	fmt.Println("PUT status:", putResp.Status)
-
-	body, err = io.ReadAll(putResp.Body)
-	if err != nil {
-		fmt.Println("Error reading body:", err)
-		return
-	}
-
-	fmt.Println(string(body))
+	return &payload, nil
 }
