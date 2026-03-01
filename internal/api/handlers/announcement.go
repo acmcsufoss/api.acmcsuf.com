72f0412f61e97cccb0e523e4c501c5e8e0d78544
Josh Holman <git@jolman.me>
2026-02-28 16:41:39 -0800

    add logging message

--- internal/api/handlers/announcement.go
+++ internal/api/handlers/announcement.go
@@ -13,6 +13,7 @@ import (
 	"github.com/acmcsufoss/api.acmcsuf.com/internal/api/services"
 	"github.com/acmcsufoss/api.acmcsuf.com/internal/dto"
 	"github.com/gin-gonic/gin"
+	"log"
 )
 
 type AnnouncementHandler struct {
@@ -131,7 +132,8 @@ func (h *AnnouncementHandler) CreateAnnouncement(c *gin.Context) {
 
 	fmt.Println("DTO ->", params, "\nDBMODEL->", dbParams)
 	// TODO: error out if required fields aren't provided
-	if err := h.announcementService.Create(ctx, dbParams); err != nil {
+	if err := h.announcementService.Create(ctx, params); err != nil {
+		log.Printf("Failed to create announcement: %v\n", err)
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"error": "Failed to create announcement",
 		})
