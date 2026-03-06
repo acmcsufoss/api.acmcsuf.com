9f412c0a31229859e12f447291185526d868f928
Josh Holman <git@jolman.me>
2026-03-06 12:04:42 -0800

    remove cli docs from swagger docs

--- internal/api/handlers/swagger.go
+++ internal/api/handlers/swagger.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	docs "github.com/acmcsufoss/api.acmcsuf.com/internal/api/docs"
-	"github.com/acmcsufoss/api.acmcsuf.com/utils"
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -13,6 +12,5 @@ func NewSwaggerHandler() gin.HandlerFunc {
 	docs.SwaggerInfo.Version = "1.0"
 	docs.SwaggerInfo.Host = "localhost:8080"
 	docs.SwaggerInfo.Schemes = []string{"http", "https"}
-	docs.SwaggerInfo.Description = utils.SwaggerDescription
 	return ginSwagger.WrapHandler(swaggerFiles.Handler)
 }
