a493f5647ebd6d60b8c9bf3742b51b0e31abbb61
Josh Holman <git@jolman.me>
2026-03-06 12:25:13 -0800

    remove hardcoded host in swagger docs

--- internal/api/handlers/swagger.go
+++ internal/api/handlers/swagger.go
@@ -1,16 +1,16 @@
 package handlers
 
 import (
-	"github.com/acmcsufoss/api.acmcsuf.com/internal/api/docs"
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
+
+	"github.com/acmcsufoss/api.acmcsuf.com/internal/api/docs"
 )
 
 func NewSwaggerHandler() gin.HandlerFunc {
 	docs.SwaggerInfo.Title = "ACM CSUF API"
 	docs.SwaggerInfo.Version = "1.0"
-	docs.SwaggerInfo.Host = "localhost:8080"
 	docs.SwaggerInfo.Schemes = []string{"http", "https"}
 	return ginSwagger.WrapHandler(swaggerFiles.Handler)
 }
