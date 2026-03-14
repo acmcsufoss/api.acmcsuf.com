6fe0f3147cc944ec11e157ca31b67ffadd2366a7
sidvasu <siddharth.vasu.ca@gmail.com>
2026-03-13 17:48:10 -0700

    Added get and update queries

--- sql/queries/workshop.sql
+++ sql/queries/workshop.sql
@@ -12,6 +12,40 @@ VALUES
 (?, ?, ?, ?, ?, ?)
 RETURNING *;
 
+-- name: GetWorkshop :one
+SELECT
+    title,
+    team,
+    semester,
+    start_at,
+    link
+FROM
+    workshop
+WHERE
+    uuid = ?;
+
+-- name: UpdateWorkshop :exec
+UPDATE workshop
+SET
+    title = COALESCE(sqlc.narg('title'), title),
+    team = COALESCE(sqlc.narg('team'), team),
+    semester = COALESCE(sqlc.narg('semester'), semester),
+    start_at = COALESCE(sqlc.narg('start_at'), start_at),
+    link = COALESCE(sqlc.narg('link'), link)
+WHERE
+    uuid = sqlc.arg('uuid');
+
+-- name: GetWorkshops :many
+SELECT
+    uuid,
+    title,
+    team,
+    semester,
+    start_at,
+    link
+FROM
+    workshop;
+
 -- name: DeleteWorkshop :exec
 DELETE FROM workshop
 WHERE uuid = ?;
