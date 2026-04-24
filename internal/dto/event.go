c2d85c0ea7d7bc7a4dc447173ff40c146c5b79f9
josh <git@jolman.me>
2026-04-23 21:56:46 -0700

    update http layer

--- internal/dto/event.go
+++ internal/dto/event.go
@@ -1,6 +1,8 @@
 package dto
 
 import (
+	"time"
+
 	"github.com/acmcsufoss/api.acmcsuf.com/internal/domain"
 	"github.com/acmcsufoss/api.acmcsuf.com/utils"
 )
@@ -14,11 +16,7 @@ type Event struct {
 	Host     string `json:"host"`
 }
 
-func (e *Event) ToDomain() domain.Event {
-	if e == nil {
-		return domain.Event{}
-	}
-
+func (e Event) ToDomain() domain.Event {
 	return domain.Event{
 		Uuid:     e.Uuid,
 		Location: e.Location,
@@ -41,10 +39,24 @@ func EventDomainToDto(e *domain.Event) Event {
 }
 
 type UpdateEvent struct {
-	Uuid     string  `json:"uuid"`
 	Location *string `json:"location"`
 	StartAt  *int64  `json:"start_at"`
 	EndAt    *int64  `json:"end_at"`
 	IsAllDay *bool   `json:"is_all_day"`
 	Host     *string `json:"host"`
+}
+
+func (e UpdateEvent) ToDomain() domain.UpdateEvent {
+	d := domain.UpdateEvent{
+		Location: e.Location,
+		IsAllDay: e.IsAllDay,
+		Host:     e.Host,
+	}
+	if e.StartAt != nil {
+		*d.StartAt = utils.UnixToTime(*e.StartAt)
+	}
+	if e.EndAt != nil {
+		*d.EndAt = utils.UnixToTime(*e.EndAt)
+	}
+	return d
 }
