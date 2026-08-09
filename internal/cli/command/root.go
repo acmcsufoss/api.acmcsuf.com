aecdbaed15949180cd5239ffc3ceef0875b69f5e
josh <git@jolman.me>
2026-08-09 08:37:16 -0700

    move commands into command package

--- internal/cli/command/root.go
+++ internal/cli/command/root.go
@@ -1,4 +1,4 @@
-package cli
+package command
 
 import (
 	"context"
@@ -9,11 +9,11 @@ import (
 	"github.com/charmbracelet/fang"
 	"github.com/spf13/cobra"
 
-	"github.com/acmcsufoss/api.acmcsuf.com/internal/cli/announcements"
+	"github.com/acmcsufoss/api.acmcsuf.com/internal/cli/command/announcements"
 	"github.com/acmcsufoss/api.acmcsuf.com/internal/cli/client"
 	"github.com/acmcsufoss/api.acmcsuf.com/internal/cli/config"
-	"github.com/acmcsufoss/api.acmcsuf.com/internal/cli/events"
-	"github.com/acmcsufoss/api.acmcsuf.com/internal/cli/officers"
+	"github.com/acmcsufoss/api.acmcsuf.com/internal/cli/command/events"
+	"github.com/acmcsufoss/api.acmcsuf.com/internal/cli/command/officers"
 )
 
 type exitCode int
